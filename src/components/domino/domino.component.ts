facca2bc17e34faf12ac581abc2f57312b3e1460
mattg95 <mattgoodman95@gmail.com>
2024-08-30 14:36:36 +0100

    allow drag and rotate

--- src/components/domino/domino.component.ts
+++ src/components/domino/domino.component.ts
@@ -1,16 +1,39 @@
 import { Component, Input } from '@angular/core';
 import { Dots } from '../dots/dots.component';
-import { Direction } from '../../types';
 import { CommonModule } from '@angular/common';
+import { CdkDrag } from '@angular/cdk/drag-drop';
+import { CdkDragStart } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'domino',
   standalone: true,
-  imports: [Dots, CommonModule],
+  imports: [Dots, CommonModule, CdkDrag],
   templateUrl: './domino.component.html',
   styleUrl: './domino.component.css',
 })
 export class Domino {
+  isVertical = false;
+  public dragging = false;
+
+  private rotate() {
+    this.isVertical = !this.isVertical;
+  }
+
+  public handleDragStart(_: CdkDragStart): void {
+    this.dragging = true;
+  }
+
+  public handleDragEnd(_: CdkDragStart): void {
+    this.dragging = false;
+  }
+
+  public handleClick(_: MouseEvent): void {
+    if (this.dragging) {
+      this.dragging = false;
+      return;
+    }
+    this.rotate();
+  }
+
   @Input() values = [0, 0];
-  @Input() direction: Direction = 'vertical';
 }
