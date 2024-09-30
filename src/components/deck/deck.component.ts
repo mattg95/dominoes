5dd770975bbd334874327fe90682d9a55fbb71e5
mattg95 <mattgoodman95@gmail.com>
2024-09-30 15:28:44 +0100

    initalise deck

--- src/components/deck/deck.component.ts
+++ src/components/deck/deck.component.ts
@@ -15,7 +15,31 @@ import { Domino } from '../domino/domino.component';
 })
 export class Deck {
   constructor(private viewContainer: ViewContainerRef) {}
+
+  public deck: number[][] = [];
+  private dotValues = Array.from({ length: 7 }, (_, i) => i);
+
+  ngAfterViewInit() {
+    console.log('here');
+    this.createDeck();
+    console.log(this.deck);
+  }
+
+  private createDeck() {
+    for (const value1 of this.dotValues) {
+      for (const value2 of this.dotValues) {
+        this.deck.push([value1, value2]);
+      }
+    }
+  }
+
   public uncoverDomino() {
-    this.viewContainer.createComponent(Domino);
+    const randomIndex = Math.floor(Math.random() * this.deck.length);
+    const [dominoValues] = this.deck.splice(randomIndex, 1);
+
+    console.log(dominoValues);
+
+    const newDomino = this.viewContainer.createComponent(Domino);
+    newDomino.instance.values = dominoValues;
   }
 }
